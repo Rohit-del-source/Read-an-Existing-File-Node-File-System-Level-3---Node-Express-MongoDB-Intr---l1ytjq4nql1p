0b597e0db214770ffc75748d6930250fd4f35488
Rohit-del-source <rohitverma18158920@gmail.com>
2024-02-19 23:05:50 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
+  const content = await fs.readFile(fileName);
+  console.log(content.toString());
+  return content.toString();
   
 };
 
 
 module.exports =  reader ;
+reader(fileName);
